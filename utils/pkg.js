d61a3a43ed40e8c5011cfc6c8955742dde713b83
martiliones <martiliones@yandex.com>
2021-08-14 20:28:03 +0600

    Add example icons and config file support

--- utils/pkg.js
+++ utils/pkg.js
@@ -2,9 +2,13 @@ const fs = require('fs');
 const path = require('path');
 const readPkg = require('read-pkg');
 
-exports.resolvePkg = (context) => {
+exports.resolveConfig = (context) => {
+  if (fs.existsSync(path.join(context, 'iconset.config.js'))) {
+    return require(path.join(context, 'iconset.config.js'));
+  }
+
   if (fs.existsSync(path.join(context, 'package.json'))) {
-    return readPkg.sync({ cwd: context });
+    return readPkg.sync({ cwd: context })?.iconset;
   }
 
   return {};
